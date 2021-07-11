e829f0509d2a64b4b8d8efeaa034d6bbc9311d0a
matthew <matthew.ng@2fi-solutions.com.hk>
2021-07-11 18:34:45 +0800

    Updates

---
+++ precache-manifest.4b73348c7e9cff6c31b08720e41690a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd758db1fe4f69259e0e445f05f75ad7",
+    "url": "/matt-react-portfolio/index.html"
+  },
+  {
+    "revision": "53eb228b1dda8993694c",
+    "url": "/matt-react-portfolio/static/css/2.dfb7c7bb.chunk.css"
+  },
+  {
+    "revision": "a7d6ecef2fb23cd34087",
+    "url": "/matt-react-portfolio/static/css/main.cce32172.chunk.css"
+  },
+  {
+    "revision": "53eb228b1dda8993694c",
+    "url": "/matt-react-portfolio/static/js/2.8fda0f37.chunk.js"
+  },
+  {
+    "revision": "efc0bc89bcf702a3aa1127d8ad15fc2e",
+    "url": "/matt-react-portfolio/static/js/2.8fda0f37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7d6ecef2fb23cd34087",
+    "url": "/matt-react-portfolio/static/js/main.141cedea.chunk.js"
+  },
+  {
+    "revision": "92369c16691402fb8db1",
+    "url": "/matt-react-portfolio/static/js/runtime-main.1db9a120.js"
+  }
+]);
